85353f214b285fbd9882f91192d2106489083784
pandhe24 <40481573+pandhe24@users.noreply.github.com>
2020-01-21 17:14:10 +0700

    final voucher tukar & fix layout 1212020

--- app/build/generated/source/buildConfig/androidTest/debug/com/pontianak/ayampakusu/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/pontianak/ayampakusu/test/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.pontianak.ayampakusu.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 29;
-  public static final String VERSION_NAME = "1.3 BETA";
+  public static final int VERSION_CODE = 30;
+  public static final String VERSION_NAME = "1.3";
 }
